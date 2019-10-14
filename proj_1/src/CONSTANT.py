2cd5259392a0ade6555ab10ce91de88fd7a68c49
xin <kratosomega@icloud.com>
2019-10-14 16:07:23 -0400

    200 training

--- proj_1/src/CONSTANT.py
+++ proj_1/src/CONSTANT.py
@@ -5,7 +5,7 @@ y_prefix = "Y"
 f_prefix = "F"
 
 # ------------------------------------------ model setup (this affect the model loading funciton)
-M = 10
+M = 20
 dim_size = 2
 dim_space = [-pi, pi]
 train_percent = 0.8
@@ -18,13 +18,13 @@ mf_space_out = [-1, 1] # domain of output mf
 # ------------------------------------------ training setup
 MaxGen = 100
 pc = 0.75
-pm = 0.5
+pm = 0.25
 er = 0.3
 
 draw_size = 50
 data_size = 1600
-rand = 0.75
-mutation_rand = 0.75
+rand = 0.50
+mutation_rand = 0.50
 shuffle_type = "random" # h, v, random
 visuailzation = True
 # ------------------------------------------
