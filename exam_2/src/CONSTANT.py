882cd0d2698aef9b4c0befbc5babded8d5f428b6
Kratos <Kratos.X.Omega@gmail.com>
2019-10-27 23:31:46 -0400

    update

--- exam_2/src/CONSTANT.py
+++ exam_2/src/CONSTANT.py
@@ -1,18 +1,13 @@
 from math import *
 
-# ------------------------------------------ model setup (this affect the model loading funciton)
 M = 200
-# ------------------------------------------
-
-# ------------------------------------------ training setup
-MaxGen = 1000
+MaxGen = 500
 pc = 0.50
 pm = 0.25
 er = 0.2
-visuailzation = True
-# ------------------------------------------
 
-#is_load_gene = True
-#is_train = False
+visuailzation = True
+
+
 
 
--- exam_2/src/_main.py
+++ exam_2/src/_main.py
@@ -11,65 +11,12 @@ def main():
 	result = ga.run()
 
 	print("-------------------------")
-	print(result["gene"])
-	print(result["fitness"])
+	print("The best pipeline sequence is: ", result["gene"])
+	print("The corresponding total distance is: ", result["fitness"])
 	print("-------------------------")
 
-
-def debug():
-	gene = route()
-	
-	print(gene)
-	print(gene.shape)
-
-
-def print_route():
-	result = np.array([
-		[0., 0., 0., 0., 1., 0., 0., 0., 0.,],
-		[0., 0., 1., 0., 0., 0., 0., 0., 0.,],
-		[0., 1., 0., 0., 0., 0., 0., 0., 0.,],
-		[0., 0., 0., 0., 0., 1., 0., 0., 0.,],
-		[0., 0., 0., 0., 0., 0., 1., 0., 0.,],
-		[0., 0., 0., 1., 0., 0., 0., 0., 0.,],
-		[0., 0., 0., 0., 0., 0., 0., 1., 0.,],
-		[0., 0., 0., 0., 0., 0., 0., 0., 1.,],
-		[0., 0., 0., 0., 0., 0., 0., 0., 0.,]
-	])
-
-	current = 5
-	for i in range(result.shape[0]):
-		print(current + 1)
-		current = np.where(result[current] == 1)[0][0]
-
-
-
-
 if __name__ == '__main__':
-    main() # main run()
-    #debug() # testing & generate dataset
-    #print_route()
-
-    #show_best_gene()
-    #generate_data_csv()
-
-
-    """
-    1. get static data set [80 / 20]
-    2. partialy repeatly train on 80%
-    3. validate on whole 20%
-    """
-
-
-
-
-
-
-
-
-
-
-
-
+    main()
 
 
 
